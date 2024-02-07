660d48f39c7e81520a895d8b91574d8f21bd4072
Hanan Adipratama <hananadipratama21@gmail.com>
2024-02-07 17:05:12 +0700

    create delete logic

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -1,2 +1,54 @@
-package id.ac.ui.cs.advprog.eshop.controller;public class ProductController {
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/create")
+    public String createProductPage(Model model){
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "createProduct";
+    }
+
+    @PostMapping("/create")
+    public String createProductPost(@ModelAttribute Product product, Model model) {
+        // Generate a unique productId using UUID
+        product.setProductId(UUID.randomUUID().toString());
+        service.create(product);
+        return "redirect:list";
+    }
+
+    @GetMapping("/list")
+    public String productListPage(Model model){
+        List<Product> allProducts = service.findAll();
+        model.addAttribute("products", allProducts);
+        return "productList";
+    }
+
+    @GetMapping("/delete/{productId}")
+    public String deleteProduct(@PathVariable String productId, Model model) {
+        Product deletedProduct = service.delete(productId);
+
+        if (deletedProduct != null) {
+            model.addAttribute("deletedProduct", deletedProduct);
+        } else {
+            model.addAttribute("error", "Product with ID " + productId + " not found.");
+        }
+
+        return "redirect:/product/list";
+    }
 }
