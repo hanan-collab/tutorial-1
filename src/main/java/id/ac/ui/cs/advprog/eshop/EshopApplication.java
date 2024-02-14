d269b009df2e97993c4531a114663ef1392a13b0
Hanan Adipratama <hananadipratama21@gmail.com>
2024-02-14 17:46:50 +0700

    fourth fixing 'This utility class has a non-private constructor'

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -3,11 +3,10 @@ package id.ac.ui.cs.advprog.eshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SuppressWarnings("PMD.NonStaticInitializer")
+@SuppressWarnings("java:S1118")
 @SpringBootApplication
 public class EshopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApplication.class, args);
 	}
-
 }
