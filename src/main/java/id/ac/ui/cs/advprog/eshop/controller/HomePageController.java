c85d6620d216c8b21f0fdd5032f609ac82df0926
Hanan Adipratama <hananadipratama21@gmail.com>
2024-02-24 18:06:57 +0700

    Updating template HTML

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomePageController {
     @GetMapping("")
     public String homePage(Model model){
-        return "homepage";
+        return "homePage";
     }
 
 }
